79226cc7a6badb8087c7369cf9a78f871b674766
ridhamp4 <143334299+ridhamp4@users.noreply.github.com>
2025-06-22 18:39:24 +0000

    Deploying to gh-pages from @ ridhamp4/ridhamp4.github.io@9c19dc96c3620a9569b4691b43e897a6c6195e14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,13 +113,13 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-graph-coarsening-with-preserved-gnn-performance",
+          title: 'Graph Coarsening with Preserved GNN Performance',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-continuous-dynamic-temporal-graph-coarsening",
+          title: 'Continuous Dynamic Temporal Graph Coarsening',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
