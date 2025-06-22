93535f357e486e7a16ddfa7487bc33d71502ec4b
ridhamp4 <143334299+ridhamp4@users.noreply.github.com>
2025-06-22 18:43:14 +0000

    Deploying to gh-pages from @ ridhamp4/ridhamp4.github.io@4734a14168d3811db0a35dae95b30754fe1ed161 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -128,8 +128,8 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-creating-dataset-for-generative-ai-of-pcb-design-amp-simulation-software",
+          title: 'Creating Dataset for Generative AI of PCB Design &amp;amp; Simulation Software',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
