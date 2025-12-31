8f0d4dab1f5122145dd44fef96d2f31e58d4f36e
ridhamp4 <143334299+ridhamp4@users.noreply.github.com>
2025-12-31 10:59:07 +0000

    Deploying to gh-pages from @ ridhamp4/ridhamp4.github.io@af8c6d6f27729aee41c4e4621e71c00d682443d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -118,13 +118,6 @@ ninja.data = [{
           window.open("https://github.com/ridhamp4", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-instagram',
         title: 'Instagram',
         section: 'Socials',
@@ -137,27 +130,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/ridhamp4", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
